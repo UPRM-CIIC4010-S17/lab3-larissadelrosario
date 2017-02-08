5a23c7ca0fd4cba81074f84ca62d3a2bc8d77c92
4015d6 <4015d6@jobo.ece.uprm.edu>
2017-02-08 14:31:49 -0400

    JFrame title was changed to Larissa. The setSize was changed to (200,200) and the setLOcation was changed to (900,400) in order to have the window centralized.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,11 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame Larissa = new JFrame("Hello, world!");
-		Larissa.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		Larissa.setLocation(900, 400);
-		Larissa.setSize(200, 200);
-		Larissa.setVisible(true);
+		JFrame myFrame = new JFrame("Larissa");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(900, 400);
+		myFrame.setSize(200, 200);
+		myFrame.setVisible(true);
+		
 		
 	}
 }
