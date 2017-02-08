e39a0bcd27be89fc4d0fc7e3d6ea0c81b48747b7
4015d6 <4015d6@jobo.ece.uprm.edu>
2017-02-08 14:26:31 -0400

    The JFrame name was changef from myFrame to Larissa. The setSize was changed from (400, 100) to (200,200) and the setLOcations was changed to (900,400) in order to have the window appear in the middle of the screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,11 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		JFrame Larissa = new JFrame("Hello, world!");
+		Larissa.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Larissa.setLocation(900, 400);
+		Larissa.setSize(200, 200);
+		Larissa.setVisible(true);
+		
 	}
 }
